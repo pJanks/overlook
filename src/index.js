d77d8d513ee5f032b991a9cdae40c1c08807e297
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-10 16:29:05 -0700

    Add form validation and fetch data

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
 import $ from 'jquery';
 import './css/base.scss';
 import domUpdates from './domUpdates'
+import Customer from './Customer'
 import './images/turing-logo.png'
 
 // fetch dataset
+
 const getUserData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
     .then(response => response.json())
-    .catch(() => {
-      window.alert('There was an error.')
-    })
+    .catch((error) => window.alert(`There was an error: ${error}.`))
 }
 
 const getRoomData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
     .then(response => response.json())
-    .catch(() => {
-      window.alert('There was an error.')
-    })
+    .catch((error) => window.alert(`There was an error: ${error}.`))
 }
 
 const getBookingData = () => {
   return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
     .then(response => response.json())
-    .catch(() => {
-      window.alert('There was an error.')
-    })
+    .catch((error) => window.alert(`There was an error: ${error}.`))
 }
 
 const getData = () => {
   Promise.all([getUserData(), getRoomData(), getBookingData()])
     .then(values => console.log(values))
-    .catch((error) => window.alert(`There was an error: ${error}`))
+    .catch((error) => window.alert(`There was an error: ${error}.`))
 }
 
-  getData()
+
+getData()
+
+$('.login').keydown(domUpdates.validateInputs)
+$('.submitLogin').click(domUpdates.changePage)
+
+export default getData;
