83cb283e990258b674d8f47fc6c172fd9e75cd2a
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-01-14 13:54:19 -0700

    Refactor to get post request to work for manager

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 import Customer from './Customer'
+import Manager from './Manager'
 
-let hotelObj, user, selectedDateAsString;
+let hotelObj, user, selectedDateAsString, foundUser;
 
 const domUpdates = {
   validateInputs: () => {
@@ -34,9 +35,9 @@ const domUpdates = {
     if (userType === 'custome' && $('.password').val() === 'overlook2019') {
       $('body').html(`
       <header class='user-greeting'>
-        Hello <span class='user-name'></span>! You have spent <span class='user-spending'></span>
+        Hello <span class='user-name'></span>! You have spent <span class='user-spending'></span> with us so far.
       </header>
-      <section class='user-bookings'>
+      <section class='bookings'>
       <h3>Your Bookings:</h3>
       </section>
       <section class='form-holder'>
@@ -59,43 +60,113 @@ const domUpdates = {
             return room
           }
         })
-        $('.user-bookings').append(`
+        $('.bookings').append(`
           <div class='booking'>Date: ${booking.date}, Room: ${booking.roomNumber}, Cost: $${roomCost.costPerNight.toFixed(2)}</div>
         `)
       })
       $('.search-date').on('click', domUpdates.getAvailableRoomsByDate)
-
     } else if (userType === 'manage' && $('.password').val() === 'overlook2019') {
-      $('body').html(`<h1>FuCk YoU</h1>`)
+      user = new Manager({
+        id: 51,
+        name: 'Johnny Cassidy'
+      }, true)
+      user.getCurrentDate()
+      user.checkRoomsAvailableToday(hotelObj.roomData.rooms, hotelObj.bookingData.bookings)
+      $('body').html(`<header class='user-greeting'>
+        Hello! Our occupancy today is <span class='occupancy'>${((user.bookedRoomsToday.length / 25) * 100).toFixed(2)}%</span> and total revenue is <span class'revenue'>$${user.seeTodaysRevenue(hotelObj.roomData.rooms, hotelObj.bookingData.bookings)}</span>.
+      </header>
+      <section class='bookings'>
+      <h3>Bookings Today, ${user.date}:</h3>
+      </section>
+      <section class='form-holder'>
+        <label class='search-user-bar'>Search for users by name:</label>
+        <input class='search-by-user' type="text" placeholder='Enter a user's name'>
+        <button class='search-user'>Search</button>
+      </section>`)
+      user.roomsAvailableToday.sort((a, b) => {
+        return a.number - b.number
+      })
+      user.roomsAvailableToday.forEach(room => {
+        $('.bookings').append(`
+          <div class='each-booked-room'>Room: ${room.number}, Cost: $${room.costPerNight.toFixed(2)}</div>
+          `)
+        $('.search-user').on('click', domUpdates.displayUser)
+      })
+    }
+  },
+
+  displayUser: () => {
+    if ($('.search-by-user').val()) {
+      let enteredName = $('.search-by-user').val()
+      foundUser = user.findBookingsByUser(hotelObj.roomData.rooms, hotelObj.bookingData.bookings, hotelObj.users.users, enteredName)
+      console.log(foundUser);
+      if (user.customerBookings.length) {
+        let userCost = hotelObj.roomData.rooms.reduce((acc, room) => {
+          user.customerBookings.forEach(booking => {
+            if (room.number === booking.roomNumber) {
+              acc += room.costPerNight
+            }
+          })
+          return acc
+        }, 0)
+        $('.bookings').html('');
+        $('.bookings').html(`${enteredName} has spent $${userCost} on these bookings: `);
+
+        user.customerBookings.forEach(booking => {
+          $('.bookings').append(`
+          <div class='each-booked-room'>Date: ${booking.date}, Room: ${booking.roomNumber}</div>
+          `)
+          $('.form-holder').html(`
+              <label class='search-bar'>Search for rooms available by date:</label>
+              <input class='search-by-date' type="text" placeholder='yyyy/mm/dd'>
+              <button class='search-date'>Search</button>`)
+            })
+      }
+      $('.search-date').on('click', domUpdates.getAvailableRoomsByDate)
     }
   },
 
   getAvailableRoomsByDate: () => {
     let i = -1;
     selectedDateAsString = $('.search-by-date').val()
-    if ($('.search-by-date').val()) {
+    user.checkAvailableDates(hotelObj.bookingData.bookings, hotelObj.roomData.rooms, selectedDateAsString, user.date)
+    if ($('.search-by-date').val() && user.availableRoomsByDate) {
       $('.form-holder').html(`
         <label class='filter-bar'>Filter by type of room:</label>
-        <input class='filter-by-type' type="text" placeholder='Enter room type here'>
-        <button class='filter-type'>Search</button>`)
-      $('.user-bookings').text(`Rooms available on ${selectedDateAsString}`)
-      user.checkAvailableDates(hotelObj.bookingData.bookings, hotelObj.roomData.rooms, selectedDateAsString, user.date)
+          <input class='filter-input' type="text" placeholder="Enter room type and press enter">
+        `)
+      $('.bookings').text(`Rooms available on ${selectedDateAsString}`)
       user.availableRoomsByDate.sort((a, b) => {
         return a.number - b.number
       }).forEach(room => {
         i++;
-        $('.user-bookings').append(`
+        $('.bookings').append(`
           <div class='availableRooms' id='${i}'>Room: ${room.number}, Room Type: ${room.roomType}, Number of Beds: ${room.numBeds}, Bed Size: ${room.bedSize}, Cost: $${room.costPerNight.toFixed(2)} <button class='book-room-button' id='${i}'>Book Now!</button></div>`)
-
       })
-      // $('.book-room-button').(domUpdates.bookARoom())
+      $('.filter-input').on('keydown', domUpdates.filterRoomsByType)
+    } else {
+      // window.alert('There are no rooms available for that day! I fierecly apologize!');
+      return 'There are no rooms available for that day! I fierecly apologize!'
     }
     $('.book-room-button').click(domUpdates.bookARoom)
   },
 
   bookARoom: (event) => {
-    user.postRoom($(event.target).attr('id'), selectedDateAsString)
-  }
+    user.postRoom($(event.target).attr('id'), selectedDateAsString, foundUser);
+    user.bookRoom(event.target.number);
+  },
+
+  filterRoomsByType: () => {
+    if ($('.filter-input').val()) {
+      $('.bookings').text('');
+      user.availableRoomsByDate.forEach(room => {
+        if (room.roomType.toLowerCase() === $('.filter-input').val().toLowerCase()) {
+          $('.bookings').append(`
+            <div class='availableRooms'>Room: ${room.number}, Room Type: ${room.roomType}, Number of Beds: ${room.numBeds}, Bed Size: ${room.bedSize}, Cost: $${room.costPerNight.toFixed(2)} <button class='book-room-button'>Book Now!</button></div>`)
+        }
+      })
+    }
+  },
 }
 
 export default domUpdates;
